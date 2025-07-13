30a15b868ec23371e8cbae228bb3446ba60fb527
alebelhepa <alejandraherrerapalacios27@gmail.com>
2025-07-13 18:22:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const cursos = [
     nombre: "NIVELACIÓN DE MATEMÁTICA",
     ciclo: "0 - Nivelación",
     creditos: 0,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000X101",
     nombre: "NIVELACIÓN DE REDACCIÓN",
     ciclo: "0 - Nivelación",
     creditos: 0,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   // 1er ciclo
   {
@@ -20,42 +22,48 @@ const cursos = [
     nombre: "Matemática I",
     ciclo: "1er ciclo",
     creditos: 3,
-    prerequisitos: ["100000X104"]
+    prerequisitos: ["100000X104"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N07C",
     nombre: "Ciudadanía y Reflexión Ética",
     ciclo: "1er ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000IN06",
     nombre: "Introducción a la Ingeniería Industrial",
     ciclo: "1er ciclo",
     creditos: 2,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000VU01",
     nombre: "Introducción a la Vida Universitaria",
     ciclo: "1er ciclo",
     creditos: 2,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N03I",
     nombre: "Inglés I",
     ciclo: "1er ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N01I",
     nombre: "Comprensión y Redacción de Textos I",
     ciclo: "1er ciclo",
     creditos: 4,
-    prerequisitos: ["100000X101"]
+    prerequisitos: ["100000X101"],
+    tipo: "obligatorio"
   },
   // 2do ciclo
   {
@@ -63,56 +71,64 @@ const cursos = [
     nombre: "Matemática II",
     ciclo: "2do ciclo",
     creditos: 4,
-    prerequisitos: ["100000I0N2"]
+    prerequisitos: ["100000I0N2"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000L02Q",
     nombre: "Laboratorio de Química General",
     ciclo: "2do ciclo",
     creditos: 0.28,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "electivo"
   },
   {
     codigo: "100000Q01G",
     nombre: "Química General",
     ciclo: "2do ciclo",
     creditos: 2.72,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I07N",
     nombre: "Principios de Algoritmos",
     ciclo: "2do ciclo",
     creditos: 2,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N09I",
     nombre: "Individuo y Medio Ambiente",
     ciclo: "2do ciclo",
     creditos: 2,
-    prerequisitos: ["100000X101"]
+    prerequisitos: ["100000X101"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N05I",
     nombre: "Inglés II",
     ciclo: "2do ciclo",
     creditos: 3,
-    prerequisitos: ["100000N03I"]
+    prerequisitos: ["100000N03I"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I05N",
     nombre: "Dibujo para Ingeniería",
     ciclo: "2do ciclo",
     creditos: 2,
-    prerequisitos: ["100000I0N2"]
+    prerequisitos: ["100000I0N2"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N04I",
     nombre: "Comprensión y Redacción de Textos II",
     ciclo: "2do ciclo",
     creditos: 4,
-    prerequisitos: ["100000N01I"]
+    prerequisitos: ["100000N01I"],
+    tipo: "obligatorio"
   },
   // 3er ciclo
   {
@@ -120,56 +136,64 @@ const cursos = [
     nombre: "Cálculo I",
     ciclo: "3er ciclo",
     creditos: 4,
-    prerequisitos: ["100000I0N3"]
+    prerequisitos: ["100000I0N3"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000IF21",
     nombre: "Economía General - IND",
     ciclo: "3er ciclo",
     creditos: 3,
-    prerequisitos: ["100000IN06"]
+    prerequisitos: ["100000IN06"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I22D",
     nombre: "Química Industrial",
     ciclo: "3er ciclo",
     creditos: 2,
-    prerequisitos: ["100000L02Q", "100000Q01G"]
+    prerequisitos: ["100000L02Q", "100000Q01G"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N06I",
     nombre: "Problemas y Desafíos en el Perú Actual",
     ciclo: "3er ciclo",
     creditos: 3,
-    prerequisitos: ["100000N09I"]
+    prerequisitos: ["100000N09I"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I04N",
     nombre: "Herramientas Informáticas para la Toma de Decisiones",
     ciclo: "3er ciclo",
     creditos: 2,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000F1I2",
     nombre: "Laboratorio de Mecánica Clásica",
     ciclo: "3er ciclo",
     creditos: 0.22,
-    prerequisitos: ["100000I0N3"]
+    prerequisitos: ["100000I0N3"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000F1I1",
     nombre: "Mecánica Clásica",
     ciclo: "3er ciclo",
     creditos: 3.78,
-    prerequisitos: ["100000I0N3"]
+    prerequisitos: ["100000I0N3"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N08I",
     nombre: "Inglés III",
     ciclo: "3er ciclo",
     creditos: 3,
-    prerequisitos: ["100000N05I"]
+    prerequisitos: ["100000N05I"],
+    tipo: "obligatorio"
   },
   // 4to ciclo
   {
@@ -177,56 +201,64 @@ const cursos = [
     nombre: "Cálculo II",
     ciclo: "4to ciclo",
     creditos: 2,
-    prerequisitos: ["100000I0N6"]
+    prerequisitos: ["100000I0N6"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI23",
     nombre: "Operaciones Unitarias y Procesos Industriales",
     ciclo: "4to ciclo",
     creditos: 4,
-    prerequisitos: ["100000I22D"]
+    prerequisitos: ["100000I22D"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000F2I2",
     nombre: "Laboratorio de Fundamentos de Electromagnetismo",
     ciclo: "4to ciclo",
     creditos: 0.22,
-    prerequisitos: ["100000F1I2", "100000F1I1", "100000I0N6"]
+    prerequisitos: ["100000F1I2", "100000F1I1", "100000I0N6"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000F2I1",
     nombre: "Fundamentos de Electromagnetismo",
     ciclo: "4to ciclo",
     creditos: 3.78,
-    prerequisitos: ["100000F1I2", "100000F1I1", "100000I0N6"]
+    prerequisitos: ["100000F1I2", "100000F1I1", "100000I0N6"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I13N",
     nombre: "Estadística Descriptiva y Probabilidades",
     ciclo: "4to ciclo",
     creditos: 3,
-    prerequisitos: ["100000I0N3"]
+    prerequisitos: ["100000I0N3"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N02C",
     nombre: "Investigación Académica",
     ciclo: "4to ciclo",
     creditos: 4,
-    prerequisitos: ["100000N04I"]
+    prerequisitos: ["100000N04I"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N10I",
     nombre: "Inglés IV",
     ciclo: "4to ciclo",
     creditos: 3,
-    prerequisitos: ["100000N08I"]
+    prerequisitos: ["100000N08I"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI22",
     nombre: "Fisicoquímica",
     ciclo: "4to ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "electivo"
   },
   // 5to ciclo
   {
@@ -234,56 +266,64 @@ const cursos = [
     nombre: "Cálculo para la Toma de Decisiones",
     ciclo: "5to ciclo",
     creditos: 2,
-    prerequisitos: ["100000I1N0"]
+    prerequisitos: ["100000I1N0"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I97N",
     nombre: "Ergonomía y Estudio del Trabajo",
     ciclo: "5to ciclo",
     creditos: 4,
-    prerequisitos: ["100000I04N"]
+    prerequisitos: ["100000I04N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000RI07",
     nombre: "Contabilidad General",
     ciclo: "5to ciclo",
     creditos: 3,
-    prerequisitos: ["100000IF21"]
+    prerequisitos: ["100000IF21"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I17N",
     nombre: "Estadística Inferencial",
     ciclo: "5to ciclo",
     creditos: 4,
-    prerequisitos: ["100000I13N"]
+    prerequisitos: ["100000I13N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000F3I2",
     nombre: "Laboratorio de Fluidos y Termodinámica",
     ciclo: "5to ciclo",
     creditos: 0.22,
-    prerequisitos: ["100000F2I2", "100000F2I1", "100000I1N0"]
+    prerequisitos: ["100000F2I2", "100000F2I1", "100000I1N0"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000F3I1",
     nombre: "Fluidos y Termodinámica",
     ciclo: "5to ciclo",
     creditos: 4.78,
-    prerequisitos: ["100000F2I2", "100000F2I1", "100000I1N0"]
+    prerequisitos: ["100000F2I2", "100000F2I1", "100000I1N0"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I27N",
     nombre: "Administración y Organización de Empresas",
     ciclo: "5to ciclo",
     creditos: 3,
-    prerequisitos: ["100000IF21"]
+    prerequisitos: ["100000IF21"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000S41V",
     nombre: "Procesos para Ingeniería",
     ciclo: "5to ciclo",
     creditos: 2,
-    prerequisitos: ["100000I13N"]
+    prerequisitos: ["100000I13N"],
+    tipo: "obligatorio"
   },
   // 6to ciclo
   {
@@ -291,56 +331,64 @@ const cursos = [
     nombre: "Teoría de Decisiones",
     ciclo: "6to ciclo",
     creditos: 3,
-    prerequisitos: ["100000I14N"]
+    prerequisitos: ["100000I14N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI01",
     nombre: "Gestión por Procesos",
     ciclo: "6to ciclo",
     creditos: 3,
-    prerequisitos: ["100000S41V"]
+    prerequisitos: ["100000S41V"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I51D",
     nombre: "Gestión de Personas",
     ciclo: "6to ciclo",
     creditos: 3,
-    prerequisitos: ["100000I27N"]
+    prerequisitos: ["100000I27N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI07",
     nombre: "Disposición de Planta",
     ciclo: "6to ciclo",
     creditos: 2,
-    prerequisitos: ["100000I97N"]
+    prerequisitos: ["100000I97N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I99N",
     nombre: "Curso Integrador I - Escuela de Industrial",
     ciclo: "6to ciclo",
     creditos: 2,
-    prerequisitos: ["100000S41V"]
+    prerequisitos: ["100000S41V"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI06",
     nombre: "Costos y Presupuestos",
     ciclo: "6to ciclo",
     creditos: 3,
-    prerequisitos: ["100000RI07"]
+    prerequisitos: ["100000RI07"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000A195",
     nombre: "Creatividad Empresarial",
     ciclo: "6to ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "electivo"
   },
   {
     codigo: "100000S10L",
     nombre: "Economía Ambiental y Sostenibilidad",
     ciclo: "6to ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "electivo"
   },
   // 7mo ciclo
   {
@@ -348,56 +396,64 @@ const cursos = [
     nombre: "Marketing",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I27N"]
+    prerequisitos: ["100000I27N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000D01T",
     nombre: "Tecnología Industrial",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI23"]
+    prerequisitos: ["100000NI23"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000TV18",
     nombre: "Investigación Operativa",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I17N"]
+    prerequisitos: ["100000I17N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI24",
     nombre: "Ingeniería Económica",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI06"]
+    prerequisitos: ["100000NI06"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I94N",
     nombre: "Herramientas de Calidad",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I13N"]
+    prerequisitos: ["100000I13N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI15",
     nombre: "Gestión de Operaciones",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I99N"]
+    prerequisitos: ["100000I99N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000ZZ29",
     nombre: "Liderazgo y Conformación de Equipos",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I51D"]
+    prerequisitos: ["100000I51D"],
+    tipo: "electivo"
   },
   {
     codigo: "100000NI08",
     nombre: "Gestión de Recursos Humanos",
     ciclo: "7mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I51D"]
+    prerequisitos: ["100000I51D"],
+    tipo: "electivo"
   },
   // 8vo ciclo
   {
@@ -405,56 +461,64 @@ const cursos = [
     nombre: "Automatización de Procesos",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: ["100000D01T"]
+    prerequisitos: ["100000D01T"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI19",
     nombre: "Logística",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI15"]
+    prerequisitos: ["100000NI15"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I72D",
     nombre: "Gestión de la Innovación",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I99N"]
+    prerequisitos: ["100000I99N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI12",
     nombre: "Sistemas de Gestión de Calidad",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: ["100000I94N"]
+    prerequisitos: ["100000I94N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI16",
     nombre: "Planeamiento y Control de Operaciones",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI15"]
+    prerequisitos: ["100000NI15"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000C17M",
     nombre: "Planeamiento Estratégico",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI01"]
+    prerequisitos: ["100000NI01"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000C0P1",
     nombre: "Content Management",
     ciclo: "8vo ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "electivo"
   },
   {
     codigo: "100000ZZ28",
     nombre: "Elementary Business English",
     ciclo: "8vo ciclo",
     creditos: 4,
-    prerequisitos: ["100000N10I"]
+    prerequisitos: ["100000N10I"],
+    tipo: "electivo"
   },
   // 9no ciclo
   {
@@ -462,56 +526,64 @@ const cursos = [
     nombre: "Formación para la Empleabilidad",
     ciclo: "9no ciclo",
     creditos: 3,
-    prerequisitos: ["100000C17M"]
+    prerequisitos: ["100000C17M"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000H01C",
     nombre: "Herramientas para la Comunicación Efectiva",
     ciclo: "9no ciclo",
     creditos: 3,
-    prerequisitos: ["100000I99N"]
+    prerequisitos: ["100000I99N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI17",
     nombre: "Simulación",
     ciclo: "9no ciclo",
     creditos: 2,
-    prerequisitos: ["100000TV18"]
+    prerequisitos: ["100000TV18"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI41",
     nombre: "Seguridad y Salud Ocupacional",
     ciclo: "9no ciclo",
     creditos: 3,
-    prerequisitos: ["100000D01T"]
+    prerequisitos: ["100000D01T"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000TV19",
     nombre: "Formulación y Evaluación de Proyectos",
     ciclo: "9no ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI24", "100000I27N"]
+    prerequisitos: ["100000NI24", "100000I27N"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI04",
     nombre: "Formación para la Investigación - Industrial",
     ciclo: "9no ciclo",
     creditos: 4,
-    prerequisitos: ["100000NI16", "100000N02C"]
+    prerequisitos: ["100000NI16", "100000N02C"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I92N",
     nombre: "Gestión de Mantenimiento",
     ciclo: "9no ciclo",
     creditos: 3,
-    prerequisitos: ["100000D01T"]
+    prerequisitos: ["100000D01T"],
+    tipo: "electivo"
   },
   {
     codigo: "100000A205",
     nombre: "Neuromarketing",
     ciclo: "9no ciclo",
     creditos: 3,
-    prerequisitos: []
+    prerequisitos: [],
+    tipo: "electivo"
   },
   // 10mo ciclo
   {
@@ -519,56 +591,64 @@ const cursos = [
     nombre: "Taller de Investigación - Industrial",
     ciclo: "10mo ciclo",
     creditos: 4,
-    prerequisitos: ["100000NI04"]
+    prerequisitos: ["100000NI04"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI13",
     nombre: "Gestión del Medio Ambiente",
     ciclo: "10mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000N09I"]
+    prerequisitos: ["100000N09I"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000I33N",
     nombre: "Gestión de Proyectos",
     ciclo: "10mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI12"]
+    prerequisitos: ["100000NI12"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000D02T",
     nombre: "Gestión de la Cadena de Abastecimiento",
     ciclo: "10mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI19"]
+    prerequisitos: ["100000NI19"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000N25I",
     nombre: "Ética Profesional",
     ciclo: "10mo ciclo",
     creditos: 2,
-    prerequisitos: ["100000C17M"]
+    prerequisitos: ["100000C17M"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000NI03",
     nombre: "Curso Integrador II - Emprendimiento Industrial",
     ciclo: "10mo ciclo",
     creditos: 4,
-    prerequisitos: ["100000I99N", "100000C17M", "100000TV19"]
+    prerequisitos: ["100000I99N", "100000C17M", "100000TV19"],
+    tipo: "obligatorio"
   },
   {
     codigo: "100000IN95",
     nombre: "Metodologías Ágiles",
     ciclo: "10mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000TV19"]
+    prerequisitos: ["100000TV19"],
+    tipo: "electivo"
   },
   {
     codigo: "100000IN89",
     nombre: "Lean Six Sigma",
     ciclo: "10mo ciclo",
     creditos: 3,
-    prerequisitos: ["100000NI16"]
+    prerequisitos: ["100000NI16"],
+    tipo: "electivo"
   }
 ];
 
@@ -593,6 +673,9 @@ function renderizar() {
 
         const divCurso = document.createElement("div");
         divCurso.className = "curso";
+        if (curso.tipo === "electivo") {
+        divCurso.classList.add("electivo");
+        }
 
         // Estado visual
         if (aprobados.includes(curso.codigo)) {
